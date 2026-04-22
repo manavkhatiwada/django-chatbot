4c6e56a72d3e1966e589f57e918303e86a11a0d0
Manav_khatiwada <manav66665555@gmail.com>
2026-04-22 15:00:10 +0545

    jwt auth part 1

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+# # Create your views here.
+# @api_view(['POST'])
+# def register(request):
+#     username = request.data.get('username')
+#     password = request.data.get('password')
+
+
+#     user = User.objects.create_user(username=username,password=password)
+#     return Response({"message":"user created"})
+
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request):
+        content = {'message': 'Hello, i am auth'}
+        return Response(content)

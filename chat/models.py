cdead505b2e45021bb3e3169d4b89bfba00f615b
Manav_khatiwada <manav66665555@gmail.com>
2026-04-26 16:09:57 +0545

    created model at chat section

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class conversation(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="conversation")
+    title = models.CharField(max_length=255,default="New chat")
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}-{self.user}"

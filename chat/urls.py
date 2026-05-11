64cfb0e56bca6caa37006027afafa27af84d63be
Manav_khatiwada <manav66665555@gmail.com>
2026-05-11 10:47:39 +0545

    added urls

--- chat/urls.py
+++ chat/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import ListConversationView,CreateConversationView,GetConversationView,SendMessageView
+from .views import ListConversationView,CreateConversationView,SendMessageView,ConversationDetailView,ConversationMessagesView
 
 
 urlpatterns = [
     path("conversations/create/",CreateConversationView.as_view(),name="create-conversation"),
     path("conversations/",ListConversationView.as_view(),name="list-conversations"),
-    path("conversation/<int:id>/", GetConversationView.as_view()),
+    path("conversation/<int:id>/", ConversationDetailView.as_view()),
     # path("conversation/<int:id>",),
+    path("conversations/<int:id>/messages/",ConversationMessagesView.as_view()),
     path("message/",SendMessageView.as_view()),
     
 
